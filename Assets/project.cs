3d078fab8dc3216f9f9fc3ad1d26eb27630539f7
qudar110913 <qudar110913@gmail.com>
2016-07-20 16:11:41 +0800

    qudar

---
+++ Assets/project.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class project : MonoBehaviour 
+{
+
+	public GameObject prefab001;
+	// Use this for initialization
+	void Start () {                }
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Fire1")) {
+			//Debug.Log ("test");
+			Instantiate(prefab001);
+		}
+	}
+}
