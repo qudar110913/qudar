e9c1ce2439529a02e44270a092b0e60f5fb27bfc
qudar110913 <qudar110913@gmail.com>
2016-07-27 16:38:09 +0800

    qudar

--- Assets/project.cs
+++ Assets/project.cs
@@ -10,9 +10,21 @@ public class project : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetButtonDown ("Fire1")) {
-			//Debug.Log ("test");
-			Instantiate(prefab001);
+		float ver = Input.GetAxis ("Vertical");
+		float hor = Input.GetAxis ("Horizontal");
+		Vector2 vec = new Vector2(ver,hor);
+		if(vec.magnitude > 0)
+		{
+			transform.position=new Vector3(transform.position.x+ver,transform.position.y,transform.position.z+hor);
 		}
 	}
 }
+
+//{
+//	{
+//		if (Input.GetButtonDown ("Fire1")) {
+//			//Debug.Log ("test");
+//			Instantiate(prefab001);
+//		}
+//	}
+//}
